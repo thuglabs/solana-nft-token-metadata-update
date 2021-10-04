b6a326ccb4767c7afbc9cf2a69b5bbae8a9dd2f4
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-10-04 22:46:28 +0300

    Remove lib directory

--- lib/constants.d.ts
+++
@@ -1,3 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-export declare const METAPLEX: PublicKey;
-export declare const CANDY_MACHINE_3D: PublicKey;
--- lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { AccountInfo } from '@solana/web3.js';
-import { Metadata } from './metaplex/classes';
-export declare const loadData: (file?: string) => string[];
-export declare function chunks<T>(array: T[], size: number): T[][];
-export declare const getMultipleAccounts: (connection: any, keys: string[], commitment: string) => Promise<{
-    keys: string[];
-    array: AccountInfo<Buffer>[];
-}>;
-export declare const getImageUrl: (meta: Metadata) => Promise<string>;
